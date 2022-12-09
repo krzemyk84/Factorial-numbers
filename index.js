847c692764c4caa7a494b4a7929a4a78c540d47f
krzemyk <113865837+krzemyk84@users.noreply.github.com>
2022-12-09 11:32:46 +0000

    the result shows all the numbers including negative values

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ var promptNumber = prompt("Please enter a number: ");
 var totalResult = factorial(promptNumber).replaceAll(",", "*")
 
 function factorial(num) {
-  var value = 1; // this sets result=1 for promptNumber < 2 
-  fact = [1]
-  for (var i = 2; i <= num; i++) // for prompted number greater than or equal 2
-    fact.push(i);
-    var factString = fact.toString();
-  return factString;
+  if (num >= 0) {
+    fact = [1] // for 1 or 0 the factorial is 1
+    for (var i = 2; i <= num; i++) // for prompted number greater than or equal 2
+      fact.push(i);
+      var factString = fact.toString();
+    return factString;
+  }
+  else {
+    fact = [-1] // for 1 or 0 the factorial is 1
+    for (var i = -2; i >= num; i--) // for prompted number greater than or equal 2
+      fact.push(i);
+      var factString = fact.toString();
+    return factString;
+  }
 }
 
 alert("Factorial of " + promptNumber + " is " + totalResult); // displays factorial of the prompted number
