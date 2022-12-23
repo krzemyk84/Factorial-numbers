522da4638907df63a33bd24a41ab2e0906c98947
krzemyk <113865837+krzemyk84@users.noreply.github.com>
2022-12-23 11:40:58 +0000

    using the second array to pass value and result as string

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 var fact = [];
+var result = [];
 var promptNumber = prompt("Please enter a number: ");
-var totalResult = factorial(promptNumber).replaceAll(",", "*")
+var totalResult = factorial(promptNumber)
+// var x = 0;
 
 function factorial(num) {
+  var value = 1;
   if (num >= 0) {
     fact = [1] // for 1 or 0 the factorial is 1
-    for (var i = 2; i <= num; i++) // for prompted number greater than or equal 2
+    for (var i = 2; i <= num; i++) {// for prompted number greater than or equal 2
+      value *= i;
       fact.push(i);
+      // alert("Value in for loop says: "+value)
+    }
+      fact.reverse();
       var factString = fact.toString();
-    return factString;
+      var factStringAsterisk = factString.replaceAll(",", "*")
+    result.push(value);
+    result.push(factStringAsterisk);
+    var returnedResult = result
+    // alert("Results says: " + result)
+    // alert("Value says: "+value)
+    return returnedResult;
   }
   else {
     fact = [-1] // for 1 or 0 the factorial is 1
-    for (var i = -2; i >= num; i--) // for prompted number greater than or equal 2
+    for (var i = -2; i >= num; i--) { // for prompted number greater than or equal 2
       fact.push(i);
+    }
       var factString = fact.toString();
     return factString;
   }
 }
-
-alert("Factorial of " + promptNumber + " is " + totalResult); // displays factorial of the prompted number
+// alert(factorial())
+alert("Result of " + promptNumber + "!: " + totalResult[1] + " = " + totalResult[0]); // displays factorial of the prompted number
